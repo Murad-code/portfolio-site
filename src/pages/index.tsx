69b4d49d4f844832a44dcf1cb34bb96c80de0d46
Murad Kamali <murad.coding@gmail.com>
2023-04-15 06:35:58 +0100

    Changes the title in the head and adds custom icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,17 @@ import About from "@/Components/About";
 import Portfolio from "@/Components/Portfolio";
 import Experience from "@/Components/Experience";
 import Contact from "@/Components/Contact";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="mb-24">
+      <div>
+        <Head>
+          <title>Murad Kamali</title>
+          <link rel="icon" href="/m.svg" />
+        </Head>
+      </div>
       <NavBar />
       <div id="Home" className="Container px-40 pt-40 text-black">
         <Hero />
